b8c18016e7d1a43a05f65684c54a41f5abaf723d
yukyyq2005 <yukyyq2005@163.com>
2019-02-20 17:46:15 +0800

    html

--- src/main/java/com/kfit/FileController.java
+++ src/main/java/com/kfit/FileController.java
@@ -1,10 +1,17 @@
 package com.kfit;
 
 
+import java.io.File;
 import java.util.Date;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 //@RequestMapping("/user")
@@ -18,4 +25,15 @@ public class FileController {
     public  Object testjson( ){
     	return  new Product(new Long(10),"dddd","dddd", new Date());
     }
+	
+	@RequestMapping(value = "upload", method = RequestMethod.POST)
+    @ResponseBody
+    public Object uploadImage(@RequestParam("head_img") MultipartFile file, HttpServletRequest request) throws Exception {
+		
+		File dest = new File("/Users/tsningning",file.getOriginalFilename());
+		file.transferTo(dest);
+		
+        return file.getOriginalFilename()+"上传成功！";
+    }
+	
 }
